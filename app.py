03852f215b07b0faab8926f8a3f046947aa2be69
Dr. Murat ALTUN <murataltun@akdeniz.edu.tr>
2026-07-27 22:12:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,17 @@ import cv2
 import numpy as np
 from PIL import Image
 
-st.title("Streamlit Görsel Grileştirme Aracı")
+st.title("Görsel Grileştirme Aracı")
 
 uploaded_file = st.file_uploader("Bir görsel yükleyin", type=["jpg", "jpeg", "png", "webp"])
 
 if uploaded_file is not None:
-    # Yüklenen dosyayı PIL Image üzerinden NumPy dizisine çevir (RGB)
     image = Image.open(uploaded_file)
     img_array = np.array(image)
 
-    # RGB -> GRAY dönüşümü
+    # RGB -> GRAY
     gray_image = cv2.cvtColor(img_array, cv2.COLOR_RGB2GRAY)
 
-    # Yan yana gösterim
     col1, col2 = st.columns(2)
     with col1:
         st.subheader("Orijinal Görsel")
